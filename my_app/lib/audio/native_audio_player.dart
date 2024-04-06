7c967d01e55511528fc73f8faa831c67999ae46c
eesso123 <25100297@lums.edu.pk>
2024-04-06 16:39:28 +0500

    Add NativeAudioPlayer class for handling audio playback

---
+++ my_app/lib/audio/native_audio_player.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/services.dart';
+
+class NativeAudioPlayer {
+  static const MethodChannel _channel =
+      MethodChannel('com.example.my_app/audio_player');
+
+  static Future<void> playAudio(String filePath) async {
+    try {
+      await _channel.invokeMethod('playAudio', {'filePath': filePath});
+    } on PlatformException catch (e) {
+      print("Failed to play audio: '${e.message}'.");
+    }
+  }
+
+  // New method to stop audio playback
+  static Future<void> stopAudio() async {
+    try {
+      await _channel.invokeMethod('stopAudio');
+    } on PlatformException catch (e) {
+      print("Failed to stop audio: '${e.message}'.");
+    }
+  }
+
+  static Future<void> dispose() async {
+    try {
+      await _channel.invokeMethod('dispose');
+    } on PlatformException catch (e) {
+      print("Failed to dispose audio player: '${e.message}'.");
+    }
+  }
+}
