15c41983ea3a41a71bc8e11448b444c56dcfe3a0
eesso123 <25100297@lums.edu.pk>
2024-04-06 17:49:04 +0500

    Add team member slideshow to About Us page
    - Import the carousel_slider package to create a slideshow of team member images
    - Create a list of team member data including names and image paths
    - Add a CarouselSlider widget to display the team member slideshow
    - Customize the appearance and behavior of the slideshow using CarouselOptions
    - Update the About Us page layout to include the team member slideshow section

--- my_app/lib/views/settings/about_us_page.dart
+++ my_app/lib/views/settings/about_us_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/components/footer.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class AboutUsPage extends StatelessWidget {
   final String username;
@@ -8,6 +9,29 @@ class AboutUsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, String>> teamMembers = [
+      {
+        'name': 'Adeen Ali Khan',
+        'image': 'pictures/adeen.png',
+      },
+      {
+        'name': 'Essa Shahid Arshad',
+        'image': 'pictures/essa.png',
+      },
+      {
+        'name': 'Muhammad Hurraira Anwer',
+        'image': 'pictures/hurraira.png',
+      },
+      {
+        'name': 'Muhammad Mehdi Changezi',
+        'image': 'pictures/mehdi.png',
+      },
+      {
+        'name': 'Mustafa Ashfaq',
+        'image': 'pictures/mustafa.png',
+      },
+    ];
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
@@ -26,7 +50,6 @@ class AboutUsPage extends StatelessWidget {
           },
         ),
       ),
-      backgroundColor: Colors.black,
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -37,7 +60,7 @@ class AboutUsPage extends StatelessWidget {
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
+                color: Colors.black,
               ),
             ),
             const SizedBox(height: 16),
@@ -46,13 +69,13 @@ class AboutUsPage extends StatelessWidget {
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
+                color: Colors.black,
               ),
             ),
             const SizedBox(height: 8),
             const Text(
               'At Idea Enhancer App, our mission is to empower individuals and teams to unleash their creative potential and bring their ideas to life. We strive to provide a user-friendly platform that fosters collaboration, innovation, and productivity.',
-              style: TextStyle(fontSize: 16, color: Colors.white),
+              style: TextStyle(fontSize: 16, color: Colors.black),
             ),
             const SizedBox(height: 16),
             const Text(
@@ -60,13 +83,13 @@ class AboutUsPage extends StatelessWidget {
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
+                color: Colors.black,
               ),
             ),
             const SizedBox(height: 8),
             const Text(
               'Founded in 2022, Idea Enhancer App was born out of a passion for innovation and a desire to make idea management accessible to everyone. Our team of experienced developers and designers came together to create a powerful yet intuitive app that simplifies the process of capturing, organizing, and executing ideas.',
-              style: TextStyle(fontSize: 16, color: Colors.white),
+              style: TextStyle(fontSize: 16, color: Colors.black),
             ),
             const SizedBox(height: 16),
             const Text(
@@ -74,7 +97,7 @@ class AboutUsPage extends StatelessWidget {
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
+                color: Colors.black,
               ),
             ),
             const SizedBox(height: 8),
@@ -83,7 +106,7 @@ class AboutUsPage extends StatelessWidget {
               '- Collaboration: We foster a culture of teamwork and encourage open communication to achieve shared goals.\n'
               '- User-Centric: We put our users at the center of everything we do, constantly striving to improve their experience.\n'
               '- Simplicity: We aim to make complex processes simple and intuitive, enabling users to focus on what matters most.',
-              style: TextStyle(fontSize: 16, color: Colors.white),
+              style: TextStyle(fontSize: 16, color: Colors.black),
             ),
             const SizedBox(height: 16),
             const Text(
@@ -91,18 +114,70 @@ class AboutUsPage extends StatelessWidget {
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
+                color: Colors.black,
               ),
             ),
             const SizedBox(height: 8),
             const Text(
               'If you have any questions, feedback, or suggestions, we\'d love to hear from you. Feel free to reach out to us at:',
-              style: TextStyle(fontSize: 16, color: Colors.white),
+              style: TextStyle(fontSize: 16, color: Colors.black),
             ),
             const SizedBox(height: 8),
             const Text(
-              'Email: info@ideaenhancerapp.com\nPhone: +1 (123) 456-7890\nAddress: 123 Innovation Street, City, Country',
-              style: TextStyle(fontSize: 16, color: Colors.white),
+              'Email: info@ideaenhancerapp.com\nPhone: +92 3056781234 \nAddress: 123 Innovation Street, City, Country',
+              style: TextStyle(fontSize: 16, color: Colors.black),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Our Team',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 8),
+            CarouselSlider(
+              items: teamMembers.map((member) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 60,
+                            backgroundImage: AssetImage(member['image']!),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            member['name']!,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+              options: CarouselOptions(
+                height: 200,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.8,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 3),
+                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                scrollDirection: Axis.horizontal,
+              ),
             ),
           ],
         ),
