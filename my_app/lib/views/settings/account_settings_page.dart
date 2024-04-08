9a6c598779f9dff15302ced5b01043d2783d3f46
eesso123 <25100297@lums.edu.pk>
2024-04-08 21:45:46 +0500

    Add an Elevated Button 'Open Google Account Settings'
    This commit adds an Elevated Button labeled 'Open Google Account Settings' to the user interface. This button is intended for cases where a user is signed in via Google. Clicking this button will redirect the user directly to the Google Account Settings website.

--- my_app/lib/views/settings/account_settings_page.dart
+++ my_app/lib/views/settings/account_settings_page.dart
@@ -1,33 +1,29 @@
 // ignore_for_file: prefer_const_constructors, deprecated_member_use
 
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart'; // Ensure you have this import
 import 'package:my_app/components/footer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_app/models/usermodel.dart';
 import 'package:my_app/common/toast.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
 
 class AccountSettingsPage extends StatefulWidget {
   final String username;
-  final bool isGoogleSignedIn;
 
-  const AccountSettingsPage({
-    Key? key,
-    required this.username,
-    this.isGoogleSignedIn = false,
-  }) : super(key: key);
+  const AccountSettingsPage({Key? key, required this.username})
+      : super(key: key);
 
   @override
   _AccountSettingsPageState createState() => _AccountSettingsPageState();
 }
 
 class _AccountSettingsPageState extends State<AccountSettingsPage> {
-  // late TextEditingController _nameController;
   late TextEditingController _emailController;
   late TextEditingController _passwordController;
   late TextEditingController _confirmPasswordController;
   var isGmailLogin = false;
-  // var nameChanged = false;
   var passwordChanged = false;
   var emailChanged = false;
   var username = "";
@@ -35,29 +31,44 @@ class _AccountSettingsPageState extends State<AccountSettingsPage> {
 
   @override
   void initState() {
+    super.initState();
+    username = widget.username;
+    _emailController = TextEditingController(text: initialEmail);
+    _passwordController = TextEditingController();
+    _confirmPasswordController = TextEditingController();
+    checkGoogleSignIn();
+  }
+
+  Future<void> checkGoogleSignIn() async {
     final user = FirebaseAuth.instance.currentUser;
     if (user != null) {
       final providerData = user.providerData;
       for (var info in providerData) {
-        if (info.providerId == 'google.com') { isGmailLogin = true; }
+        if (info.providerId == 'google.com') {
+          setState(() {
+            isGmailLogin = true;
+          });
+          break;
+        }
       }
     }
-    print("isGmailLogin ... $isGmailLogin");
-    super.initState();
-    _nameController = TextEditingController(
-        text: widget.username); // Assuming username is the name
-    _emailController = TextEditingController(text: 'email@example.com');
-    _passwordController = TextEditingController();
-    _confirmPasswordController = TextEditingController();
   }
 
   @override
   void dispose() {
-    // _nameController.dispose();
     _emailController.dispose();
     _passwordController.dispose();
     _confirmPasswordController.dispose();
     super.dispose();
+  }
+
+  Future<void> _launchGoogleAccountSettings() async {
+    const url = 'https://myaccount.google.com/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 
   @override
@@ -65,69 +76,107 @@ class _AccountSettingsPageState extends State<AccountSettingsPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: const Text(
-          'Account Settings',
-          style: TextStyle(color: Colors.white),
-        ),
+        title: const Text('Account Settings',
+        style: TextStyle(color: Colors.white),
+      ),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () => Navigator.pop(context),
         ),
       ),
-      backgroundColor: const Color(0xFFFFE6C9),
+      backgroundColor: Colors.grey[900],
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             _buildSectionTitle('Profile Information'),
-            _buildTextField(
-              controller: _nameController,
-              labelText: 'Name',
-              icon: Icons.person,
-            ),
-            _buildTextField(
-              controller: _emailController,
-              labelText: 'Email',
-              icon: Icons.email,
-            ),
-            _buildSectionTitle('Change Password'),
-            _buildTextField(
-              controller: _passwordController,
-              labelText: 'New Password',
-              icon: Icons.lock,
-              obscureText: true,
-            ),
-            _buildTextField(
-              controller: _confirmPasswordController,
-              labelText: 'Confirm Password',
-              icon: Icons.lock_outline,
-              obscureText: true,
-            ),
+            !isGmailLogin
+                ? _buildTextField(
+                    controller: _emailController,
+                    labelText: 'Email',
+                    icon: Icons.email,
+                  )
+                : Container(),
+            !isGmailLogin ? _buildSectionTitle('Change Password') : Container(),
+            !isGmailLogin
+                ? _buildTextField(
+                    controller: _passwordController,
+                    labelText: 'New Password',
+                    icon: Icons.lock,
+                    obscureText: true,
+                  )
+                : Container(),
+            !isGmailLogin
+                ? _buildTextField(
+                    controller: _confirmPasswordController,
+                    labelText: 'Confirm Password',
+                    icon: Icons.lock_outline,
+                    obscureText: true,
+                  )
+                : Container(),
+            isGmailLogin
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'You are logged in with your Google account. To change your email, password, or username, please visit your Google account settings.',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            _launchGoogleAccountSettings();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 12),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: Text(
+                            'Open Google Account Settings',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : Container(),
             const SizedBox(height: 24),
-            !isGmailLogin ? Center(
-              child: ElevatedButton(
-                onPressed: () async {
-                    if(checkifvalidinput()){
-                      print("changed fields ... (email,pass) : $emailChanged, $passwordChanged");
-                      await updateUserInfo(username,_emailController.text,_passwordController.text,emailChanged,passwordChanged);
-                      
-                    }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-                child: const Text(
-                  'Save Changes',
-                  style: TextStyle( color: Colors.white, fontSize: 18),
-                ),
-              ),
-            ) : Text(""),
+            !isGmailLogin
+                ? Center(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (checkifvalidinput()) {
+                          await updateUserInfo(
+                              username,
+                              _emailController.text,
+                              _passwordController.text,
+                              emailChanged,
+                              passwordChanged);
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: const Text(
+                        'Save Changes',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                  )
+                : Container(),
           ],
         ),
       ),
@@ -140,7 +189,7 @@ class _AccountSettingsPageState extends State<AccountSettingsPage> {
         child: Text(
           title,
           style: const TextStyle(
-              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
         ),
       );
 
@@ -153,12 +202,13 @@ class _AccountSettingsPageState extends State<AccountSettingsPage> {
     return TextField(
       controller: controller,
       obscureText: obscureText,
+      style: const TextStyle(color: Colors.white),
       decoration: InputDecoration(
-        prefixIcon: icon != null ? Icon(icon, color: Colors.black) : null,
+        prefixIcon: icon != null ? Icon(icon, color: Colors.white70) : null,
         labelText: labelText,
-        labelStyle: const TextStyle(color: Colors.black),
+        labelStyle: const TextStyle(color: Colors.white70),
         enabledBorder: const UnderlineInputBorder(
-          borderSide: BorderSide(color: Colors.black),
+          borderSide: BorderSide(color: Colors.white70),
         ),
         focusedBorder: const UnderlineInputBorder(
           borderSide: BorderSide(color: Colors.deepPurple),
@@ -167,25 +217,39 @@ class _AccountSettingsPageState extends State<AccountSettingsPage> {
     );
   }
 
-  bool checkifvalidinput(){
-    if (_passwordController.text != "" || _confirmPasswordController.text != "" ) { passwordChanged = true; }
-    if (_emailController.text != initialEmail)                                    { emailChanged = true; }
-    if(passwordChanged == false && emailChanged == false){
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Can only update profile info if something has changed'),duration: Duration(seconds: 2),));
+  bool checkifvalidinput() {
+    if (_passwordController.text != "" ||
+        _confirmPasswordController.text != "") {
+      passwordChanged = true;
+    }
+    if (_emailController.text != initialEmail) {
+      emailChanged = true;
+    }
+    if (passwordChanged == false && emailChanged == false) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Can only update profile info if something has changed'),
+        duration: Duration(seconds: 2),
+      ));
       return false;
     }
 
-    final emailRegex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$',);
-    final passwordRegex = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*]).{8,}$',);
+    final emailRegex = RegExp(
+      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$',
+    );
+    final passwordRegex = RegExp(
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*]).{8,}$',
+    );
 
     if (!emailRegex.hasMatch(_emailController.text)) {
       showerrormsg(message: "Please enter a valid email address");
       return false;
     }
 
-    if(passwordChanged){
+    if (passwordChanged) {
       if (!passwordRegex.hasMatch(_passwordController.text)) {
-        showerrormsg(message:"Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character.");
+        showerrormsg(
+            message:
+                "Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character.");
         return false;
       }
 
@@ -195,7 +259,10 @@ class _AccountSettingsPageState extends State<AccountSettingsPage> {
       }
     }
 
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Form is valid and processing data'),duration: Duration(seconds: 2),));
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('Form is valid and processing data'),
+      duration: Duration(seconds: 2),
+    ));
     return true;
   }
 }
