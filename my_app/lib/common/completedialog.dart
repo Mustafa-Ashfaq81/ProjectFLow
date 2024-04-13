5c60c04769eb5a8f4b44c82183652a112f47ed57
eesso123 <25100297@lums.edu.pk>
2024-04-14 02:31:57 +0500

    Refactor: Clean Commenting and Code principles applied to common folder files

--- my_app/lib/common/completedialog.dart
+++ my_app/lib/common/completedialog.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
-  
+
+/*
+
+This file has a the functionality of a confirmation dialog to confirm completion of a subtask.
+
+*/
+
   void showCompleteConfirmationDialog(BuildContext context, Function completeTask){
     showDialog(
-      context: context,
+      context: context, // [context] The BuildContext in which to show the dialog.
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text("Confirm"),
+          title: const Text("Confirm"), 
           content: const Text("Are you sure you want to mark this subtask as completed?"),
           actions: <Widget>[
             TextButton(
--- my_app/lib/common/logoutdialog.dart
+++ my_app/lib/common/logoutdialog.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
 
 
-Future<bool> showLogOutDialog(BuildContext context) {
+/*
+
+This file has a method which displays a confirmation dialog asking the user if they are sure they want to log out.
+This dialog is used to confirm the user's intention to log out of the application,
+preventing accidental logouts. It returns a Future that completes with a boolean indicating the user's choice
+
+*/
+
+
+Future<bool> showLogOutDialog(BuildContext context) 
+{
   return showDialog<bool>(
     context: context,
-    builder: (context) {
+    builder: (context) 
+    {
       return AlertDialog(
         title: const Text('Log out'),
         content: const Text('Are you sure you want to log out?'),
         actions: [
           TextButton(
-              onPressed: () {
+              onPressed: () 
+              {
                 Navigator.of(context).pop(false);
               },
               child: const Text('Cancel')),
           TextButton(
-              onPressed: () {
+              onPressed: () 
+              {
                 Navigator.of(context).pop(true);
               },
               child: const Text('Log out')),
